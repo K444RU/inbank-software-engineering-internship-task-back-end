7bef2c239e077a659bcefb8aff85703ff4998128
olegtrofimov <olegtrofimov96@gmail.com>
2024-04-19 14:46:34 +0300

    Initial commit

---
+++ src/main/java/com/inbank/decisionengine/InbankSoftwareEngineeringInternshipTaskBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.inbank.decisionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InbankSoftwareEngineeringInternshipTaskBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InbankSoftwareEngineeringInternshipTaskBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inbank/decisionengine/InbankSoftwareEngineeringInternshipTaskBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbank.decisionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbankSoftwareEngineeringInternshipTaskBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
