fce8d01ca658753f713eb866830d5a4521c4b631
olegtrofimov <olegtrofimov96@gmail.com>
2024-04-20 15:27:46 +0300

    INB | service class logic
    -implemented the calculateLoanDecision service class logic:
    •it takes the personal code, loan amount, and loan period as input parameters and calculates the decision regarding the loan application
    • checks if the loan amount and loan period are within acceptable bounds
    • it creates a Loan object with the provided parameters.
    • it retrieves the credit modifier from the CREDIT_MODIFIERS map based on the personal code. If the personal code is not found, it returns a negative decision response.
    • if the credit modifier indicates that the person has debt, it returns a negative decision response with a specific message. Otherwise, it calculates the credit score, adjusts the loan amount based on the credit score, and determines the approved loan amount and period.
    • if the approved loan amount is still below the minimum loan amount, it finds a suitable loan period where the credit score meets the minimum threshold.
    • it returns a decision response indicating whether the loan application is approved or not, along with the approved loan amount and period.

--- src/main/java/com/inbank/decisionengine/loan/controller/LoanDecisionController.java
+++ src/main/java/com/inbank/decisionengine/loan/controller/LoanDecisionController.java
@@ -5,6 +5,7 @@ import com.inbank.decisionengine.loan.service.LoanDecisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,11 @@ public class LoanDecisionController {
     public LoanDecisionController(LoanDecisionService loanDecisionService) {
         this.loanDecisionService = loanDecisionService;
     }
-
+    @GetMapping("/decision")
     public ResponseEntity<?> getLoanDecision(@RequestParam String personalCode,
                                              @RequestParam int loanAmount,
                                              @RequestParam int loanPeriod){
-        LoanDecisionResponse loanDecisionResponse = loanDecisionService.calculate(personalCode, loanAmount, loanPeriod);
+        LoanDecisionResponse loanDecisionResponse = loanDecisionService.calculateLoanDecision(personalCode, loanAmount, loanPeriod);
         return new ResponseEntity<>(loanDecisionResponse, HttpStatus.OK);
     }
 
